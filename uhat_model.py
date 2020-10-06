011c357590b4813cff279d674f2a2afaf1197f22
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2020-10-06 14:36:08 -0700

    Divide npz file parts

--- uhat_model.py
+++ uhat_model.py
@@ -1,7 +1,13 @@
 import numpy as np
 from PIL import Image
 
-image = Image.open('UHaT/data/data/characters_test_set/alif/alif (1).jpg')
-data = np.asarray(image)
-
-print(data.shape)
+with np.load('UHaT/uhat_dataset.npz') as data:
+    train_chars = data['x_chars_train']
+    train_digits = data['x_digits_train']
+    test_chars = data['x_chars_test']
+    test_digits = data['x_digits_test']
+    train_chars_labels = data['y_chars_train']
+    train_digits_labels = data['y_digits_train']
+    test_chars_labels = data['y_chars_test']
+    test_digits_labels = data['y_digits_test']
+    print(len(train_chars_labels))
