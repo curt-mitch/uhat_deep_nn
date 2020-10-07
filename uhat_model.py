04d624c6c19cb92ae132adcdcde4bb9310068cc4
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2020-10-07 14:35:43 -0700

    Concat character and digit data

--- uhat_model.py
+++ uhat_model.py
@@ -1,12 +1,19 @@
 import numpy as np
 from PIL import Image
 
-with np.load('UHaT/uhat_dataset.npz') as data:
-    train_chars = data['x_chars_train']
-    train_digits = data['x_digits_train']
-    test_chars = data['x_chars_test']
-    test_digits = data['x_digits_test']
+with np.load('uhat_dataset.npz') as data:
+    train_chars_data = data['x_chars_train']
+    train_digits_data = data['x_digits_train']
+    test_chars_data = data['x_chars_test']
+    test_digits_data = data['x_digits_test']
     train_chars_labels = data['y_chars_train']
     train_digits_labels = data['y_digits_train']
     test_chars_labels = data['y_chars_test']
     test_digits_labels = data['y_digits_test']
+
+# merge character and digit data
+train_data = np.concatenate([train_chars_data, train_digits_data], axis=0)
+test_data = np.concatenate([test_chars_data, test_digits_data], axis=0)
+train_labels = np.concatenate([train_chars_labels, train_digits_labels])
+test_labels = np.concatenate([test_chars_labels, test_digits_labels])
+
