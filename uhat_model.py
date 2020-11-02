930c90ac11d5c59f4a17a98633403a5ffdd5c60f
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2020-11-02 14:43:25 -0800

    Loss functions; hyperparameters

--- uhat_model.py
+++ uhat_model.py
@@ -1,5 +1,5 @@
 import numpy as np
-from PIL import Image
+# from PIL import Image
 
 with np.load('uhat_dataset.npz') as data:
     train_chars_data = data['x_chars_train']
@@ -28,5 +28,28 @@ test_data = test_data.reshape(len(test_data), 28*28)
 # create one-hot encodings for labels
 one_hot_labels = np.zeros((len(train_labels), 50))
 
-for i,l in enumerate(train_labels):
-  one_hot_labels[i][l] = 1
+for i, l in enumerate(train_labels):
+    one_hot_labels[i][l] = 1
+train_labels = one_hot_labels
+
+# set up loss function and hyperparameters
+np.random.seed(1)
+
+
+def relu(x):
+    '''return x if x > 0, otherwise return 0'''
+    return (x >= 0) * x
+
+
+def relu2deriv(x):
+    '''derivative of relu: return 1 if x > 0, otherwise return 0'''
+    return x >= 0
+
+
+alpha = 0.005
+iterations = 350
+hidden_size = 40
+pixels_per_image = 28*28
+num_labels = 50
+weights_0_1 = 0.2 * np.random.random((pixels_per_image, hidden_size)) - 0.1
+weights_1_2 = 0.2 * np.random.random((hidden_size, num_labels)) - 0.1
