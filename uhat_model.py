a14071a8f49f58506765ccc21ba210a4fec67613
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2020-12-21 16:57:16 -0800

    Misc code cleanup

--- uhat_model.py
+++ uhat_model.py
@@ -1,6 +1,5 @@
 import sys
 import numpy as np
-# from PIL import Image
 
 with np.load('uhat_dataset.npz') as data:
     train_data = data['x_chars_train']
@@ -24,10 +23,9 @@ for i, l in enumerate(test_labels):
     one_hot_test_labels[i][l] = 1
 test_labels = one_hot_test_labels
 
+
 # set up loss function and hyperparameters
 np.random.seed(1)
-
-# TODO: https://stackoverflow.com/questions/7559595/python-runtimewarning-overflow-encountered-in-long-scalars
 
 
 def relu(x):
@@ -35,24 +33,26 @@ def relu(x):
     return (x >= 0) * x
 
 
-def relu2deriv(x):
-    '''derivative of relu: return 1 if x > 0, otherwise return 0'''
-    return x >= 0
+def relu2deriv(output):
+    '''derivative of relu: return 1 if output >= 0, otherwise return 0'''
+    return output >= 0
 
 
 alpha = 0.005
 # iterations = 350
-iterations = 1
+iterations = 50
 hidden_size = 40
 pixels_per_image = 28*28
 num_labels = 40
 weights_0_1 = 0.2 * np.random.random((pixels_per_image, hidden_size)) - 0.1
 weights_1_2 = 0.2 * np.random.random((hidden_size, num_labels)) - 0.1
 
+# train model
 for j in range(iterations):
     error = 0.0
     correct_count = 0
-    for i in range(1000):
+
+    for i in range(len(train_data)):
         layer_0 = train_data[i:i+1]
         layer_1 = relu(np.dot(layer_0, weights_0_1))
         layer_2 = np.dot(layer_1, weights_1_2)
@@ -64,8 +64,6 @@ for j in range(iterations):
             * relu2deriv(layer_1)
         weights_1_2 += alpha * np.dot(layer_1.T, layer_2_delta)
         weights_0_1 += alpha * np.dot(layer_0.T, layer_1_delta)
-        if i % 1000 == 0:
-            print('weights', weights_0_1)
     sys.stdout.write("\r" +
                      " I:" + str(j) +
                      " Error:" + str(error / float(len(train_data)))[0:5] +
